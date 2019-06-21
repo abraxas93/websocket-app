a8f6405c53b0fcb9e641799ff5e4f5a5ac5fc567
abraxas93 <savchyk.a@gmail.com>
2019-06-21 17:28:36 +0300

    created redis client

---
+++ src/redis.js
@@ -0,0 +1,22 @@
+
+import redis from 'redis';
+import promise from 'bluebird';
+import env from 'dotenv';
+
+env.config();
+
+promise.promisifyAll(redis.RedisClient.prototype);
+promise.promisifyAll(redis.Multi.prototype);
+
+export default () => new Promise((resolve, reject) => {
+  const connector = redis.createClient(process.env.REDIS_HOST);
+
+  connector.on('error', () => {
+    reject(new Error('Redis Connection failed'));
+  });
+
+  connector.on('connect', () => {
+    console.log('connected');
+    resolve(connector);
+  });
+});
