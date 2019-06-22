ce26d2488e81e04906b70ade663adcf2978597b5
abraxas93 <savchyk.a@gmail.com>
2019-06-22 13:09:17 +0300

    add some views files

--- src/index.js
+++ src/index.js
@@ -11,11 +11,49 @@ env.config();
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../public/views'));
+app.set('views', path.join(__dirname, './public/views'));
 app.set('view engine', 'pug');
 
 // static files folder setup
-app.use(express.static(path.join(__dirname, '../public/assets')));
+app.use(express.static(path.join(__dirname, './public/assets')));
 
 // Middleware to parse request body
 app.use(bodyParser.urlencoded({ extended: true }));
+
+redisClient().then(
+  (res) => {
+    // App routes
+    app.get('/', routes.home);
+    app.get('/chat/:username', routes.chatRoom);
+    app.get('/messages', routes.messages);
+    app.get('/users', routes.users);
+    app.post('/user', routes.createUser);
+    app.delete('/user', routes.deleteUser);
+    app.post('/message', routes.createMessage);
+
+    // Start the server
+    const server = app.listen(process.env.PORT, () => {
+      console.log('Server Started');
+    });
+
+    const io = socket.listen(server);
+
+    // //listen and emit messages and user events (leave or join) using socket.io
+    io.on('connection', (socket) => {
+      // subscribe to Pub/Sub channels
+      res.subscribe('chatMessages');
+      res.subscribe('activeUsers');
+
+      res.on('message', (channel, message) => {
+        if (channel === 'chatMessages') {
+          socket.emit('message', JSON.parse(message));
+        } else {
+          socket.emit('users', JSON.parse(message));
+        }
+      });
+    });
+  },
+  (err) => {
+    console.log('Redis connection failed: ', err);
+  }
+);
